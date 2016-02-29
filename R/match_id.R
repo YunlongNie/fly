7e8568fd976e35ecb024d27b9e3076a143a086ac
YunlongNie <yunlong.nie@yahoo.ca>
2016-02-28 17:11:47 -0800

    add a function mergeIDs

--- R/match_id.R
+++ R/match_id.R
@@ -3,6 +3,7 @@
 #' @param input_ids a vector contains either flybase ID, CG_ID or genesymbol; can be mixed
 #' @export
 #' @import dplyr 
+#' @import tidyr
 #' @examples
 #' \dontrun{
 #' input_ids = c("CG11880", "tin", "CG6404","XXXXXXX","d4", "Traf6")
@@ -11,9 +12,8 @@
 
 matched_id = function(input_ids)
 {
-library(dplyr);library(tidyr)
 data(FlyBase_IDs)
-ID_data=  FlyBase_IDs%>%rename(CG_ID=submitted_id,flybaseID = current_id, genesymbol = current_symbol)%>%dplyr::select(-converted_id)%>%unique
+ID_data=  FlyBase_IDs%>%dplyr::rename(CG_ID=submitted_id,flybaseID = current_id, genesymbol = current_symbol)%>%dplyr::select(-converted_id)%>%unique
 allIDs= ID_data%>%unite(allnames,CG_ID,flybaseID,genesymbol,sep=";")
 output0 = ldply(input_ids,function(x) data.frame(input = x ,match = any(grepl(x,allIDs$allnames,fixed=TRUE))))
 
@@ -21,7 +21,7 @@ output0 = ldply(input_ids,function(x) data.frame(input = x ,match = any(grepl(x,
 output = ldply(output0$input[output0$match],function(x) data.frame(input = x ,match = grep(x,allIDs$allnames,value=TRUE,
 fixed=TRUE)))
 
-output%>%separate(match,c("CG_ID","flybaseID","genesymbol"),sep=";")%>%filter(input==CG_ID|input==flybaseID|input==genesymbol)
+output%>%separate(match,c("CG_ID","flybaseID","genesymbol"),sep=";")%>%dplyr::filter(input==CG_ID|input==flybaseID|input==genesymbol)
 
 
 }
