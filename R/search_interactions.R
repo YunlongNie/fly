0cd81a10bb022abf155092fdfe20acbf36333888
YunlongNie <yunlong.nie@yahoo.ca>
2016-02-28 18:37:27 -0800

    add the function search_interactions

--- R/search_interactions.R
+++ R/search_interactions.R
@@ -2,7 +2,6 @@
 #'
 #' @param input_interactions data.frame with two cols
 #' @param type interaction type one of 'all',"ppi","rna","tf","generic"
-#' @param type interaction type one of 'all',"ppi","rna","tf","generic"
 #' @export
 #' @import dplyr 
 #' @import tidyr
@@ -24,63 +23,7 @@ source = type
 
 if (type=="all") { source = c("ppi","rna","tf","genetic") } 
 
-search_each = function(s,data_folder,input_interactions=input_int)
-{
-
-names(input_interactions) = c('id1','id2')	
-if (s!='tf')
-{
-input_interactions2 = input_interactions[,c("id2","id1")];
-names(input_interactions2) = c('id1','id2')
-input_interactions = rbind.data.frame(input_interactions,input_interactions2)
-}
-
-
-
-if (s=="ppi")
-{
-data(flybase_ppi)
-
-res = input_interactions%>%merge(.,flybase_ppi,by=c("id1","id2"))
-
-res
-
-}
-
-if (s=="rna")
-{
-
-data(rna_gene)
-
-fun <- function(x, y){ 
-rna1 = as.character(subset(rna_gene,FLY_TARGET_GENE%in%x)$RNA_SYMBOL)
-rna2 = as.character(subset(rna_gene,FLY_TARGET_GENE%in%y)$RNA_SYMBOL)
-data.frame(rna=intersect(rna1,rna2))
-}
-
-res = input_interactions%>%rowwise()%>%do(rna = fun(.$id1[1],.$id2[1]))%>%mutate(n=nrow(rna),rna = paste0(rna[,1],collapse="\\"))%>%select(n,rna)%>%cbind.data.frame(input_interactions,.)
-
-
-}
-
-if (s=="tf")
-{
-data(tf_gene)
-res = input_interactions%>%merge(.,tf_gene,by=c("id1","id2"))
-}
-
-if (s=="genetic")
-{
-
-data(fly_genetic_interactions)
-fly_genetic_interactions= fly_genetic_interactions%>%rename(id1 = FLY_GENE1,id2=FLY_GENE2)
-res = input_interactions%>%merge(.,fly_genetic_interactions,by=c("id1","id2"))
-}
-
-return(res)
-}
-
-output = source%>%data.frame(type=.)%>%group_by(type)%>%do(outdata=search_each(.$type,data_folder=data_folder,input_interactions=input_int))
+output = source%>%data.frame(type=.)%>%group_by(type)%>%do(outdata=search_each(.$type,input_interactions=input_int))
 
 
 return(output)
