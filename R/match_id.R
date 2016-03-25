2a62c0cd94ba277bebe717d7054f298c03b9e96d
YunlongNie <yunlong.nie@yahoo.ca>
2016-03-24 23:44:49 -0700

    add plyr into namespace

--- R/match_id.R
+++ R/match_id.R
@@ -4,6 +4,7 @@
 #' @export
 #' @import dplyr 
 #' @import tidyr
+#' @importFrom plyr ldply
 #' @examples
 #' \dontrun{
 #' input_ids = c("CG11880", "tin", "CG6404","XXXXXXX","d4", "Traf6")
@@ -15,10 +16,10 @@ matched_id = function(input_ids)
 data(FlyBase_IDs)
 ID_data=  FlyBase_IDs%>%dplyr::rename(CG_ID=submitted_id,flybaseID = current_id, genesymbol = current_symbol)%>%dplyr::select(-converted_id)%>%unique
 allIDs= ID_data%>%unite(allnames,CG_ID,flybaseID,genesymbol,sep=";")
-output0 = ldply(input_ids,function(x) data.frame(input = x ,match = any(grepl(x,allIDs$allnames,fixed=TRUE))))
+output0 =  plyr::ldply(input_ids,function(x) data.frame(input = x ,match = any(grepl(x,allIDs$allnames,fixed=TRUE))))
 
 
-output = ldply(output0$input[output0$match],function(x) data.frame(input = x ,match = grep(x,allIDs$allnames,value=TRUE,
+output = plyr::ldply(output0$input[output0$match],function(x) data.frame(input = x ,match = grep(x,allIDs$allnames,value=TRUE,
 fixed=TRUE)))
 
 output%>%separate(match,c("CG_ID","flybaseID","genesymbol"),sep=";")%>%dplyr::filter(input==CG_ID|input==flybaseID|input==genesymbol)
